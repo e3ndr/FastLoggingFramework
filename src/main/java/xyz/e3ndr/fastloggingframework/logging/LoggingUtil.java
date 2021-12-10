ac7470cd3cfd3293e3b50406b98c368bed5de3e2
Ender <33337309+e3ndr@users.noreply.github.com>
2021-12-10 11:21:16 -0600

    Auto convert throwable and arrays to useable strings when they're used as format args.

--- src/main/java/xyz/e3ndr/fastloggingframework/logging/LoggingUtil.java
+++ src/main/java/xyz/e3ndr/fastloggingframework/logging/LoggingUtil.java
@@ -2,6 +2,7 @@ package xyz.e3ndr.fastloggingframework.logging;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Array;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -15,11 +16,39 @@ public class LoggingUtil {
             return "null";
         } else if (object instanceof Throwable) {
             return getExceptionStack((Throwable) object);
+        } else if (object.getClass().isArray()) {
+            return arrayAsString(object);
         } else if ((args == null) || (args.length == 0)) {
             return String.valueOf(object);
         } else {
+            // Loop through the args and...
+            // - replace Throwables with their stacktraces
+            // - replace Arrays with their toString versions.
+            for (int argIndex = 0; argIndex < args.length; argIndex++) {
+                Object arg = args[argIndex];
+
+                if (arg instanceof Throwable) {
+                    args[argIndex] = getExceptionStack((Throwable) arg);
+                } else if (arg.getClass().isArray()) {
+                    args[argIndex] = arrayAsString(arg);
+                }
+            }
+
             return String.format(String.valueOf(object), args);
         }
+    }
+
+    public static String arrayAsString(@NonNull Object array) {
+        int arrLen = Array.getLength(array);
+        String[] asString = new String[arrLen];
+
+        for (int idx = 0; idx < arrLen; idx++) {
+            Object o = Array.get(array, idx);
+
+            asString[idx] = String.valueOf(o);
+        }
+
+        return '[' + String.join(", ", asString) + ']';
     }
 
     @SneakyThrows
