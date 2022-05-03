4a057d7746f536f7698309754a97b96631bd068f
Ender <33337309+e3ndr@users.noreply.github.com>
2022-05-03 11:50:21 -0500

    Remove some invalid characters.

--- src/main/java/xyz/e3ndr/fastloggingframework/logging/LogColor.java
+++ src/main/java/xyz/e3ndr/fastloggingframework/logging/LogColor.java
@@ -5,39 +5,39 @@ import lombok.Getter;
 @Getter
 public enum LogColor {
     // @formatter: offs
-    RESET("r", "#AAAAAA", "[039m[022m[024m[023m[029m"),
+    RESET("r", "#AAAAAA", "\u001b[039m\u001b[022m\u001b[024m\u001b[023m\u001b[029m"),
     DEFAULT(RESET),
 
-    BOLD("l", "", "[01m"),
-    UNDERLINE("n", "", "[04m"),
-    ITALIC("o", "", "[03m"),
-    STRIKETHROUGH("m", "", "[09m"),
+    BOLD("l", "", "\u001b[01m"),
+    UNDERLINE("n", "", "\u001b[04m"),
+    ITALIC("o", "", "\u001b[03m"),
+    STRIKETHROUGH("m", "", "\u001b[09m"),
 
-    RED("4", "#AA0000", "[031m"),
-    LIGHT_RED("c", "#FF5555", "[091m"),
+    RED("4", "#AA0000", "\u001b[031m"),
+    LIGHT_RED("c", "#FF5555", "\u001b[091m"),
 
-    YELLOW("6", "#FFAA00", "[033m"),
-    LIGHT_YELLOW("e", "#FFFF55", "[093m"),
+    YELLOW("6", "#FFAA00", "\u001b[033m"),
+    LIGHT_YELLOW("e", "#FFFF55", "\u001b[093m"),
 
-    GREEN("2", "#00AA00", "[032m"),
-    LIGHT_GREEN("a", "#55FF55", "[092m"),
+    GREEN("2", "#00AA00", "\u001b[032m"),
+    LIGHT_GREEN("a", "#55FF55", "\u001b[092m"),
 
-    AQUA("3", "#00AAAA", "[036m"),
-    LIGHT_AQUA("b", "#55FFFF", "[096m"),
+    AQUA("3", "#00AAAA", "\u001b[036m"),
+    LIGHT_AQUA("b", "#55FFFF", "\u001b[096m"),
 
-    BLUE("1", "#0000AA", "[034m"),
-    LIGHT_BLUE("9", "#5555FF", "[094m"),
+    BLUE("1", "#0000AA", "\u001b[034m"),
+    LIGHT_BLUE("9", "#5555FF", "\u001b[094m"),
 
-    MAGENTA("5", "#AA00AA", "[035m"),
-    LIGHT_MAGENTA("d", "#FF55FF", "[095m"),
+    MAGENTA("5", "#AA00AA", "\u001b[035m"),
+    LIGHT_MAGENTA("d", "#FF55FF", "\u001b[095m"),
 
-    DARK_GREY("8", "#555555", "[090m"),
-    DARK_GRAY("8", "#555555", "[090m"),
-    GREY("7", "#AAAAAA", "[039m"),
-    GRAY("7", "#AAAAAA", "[039m"),
+    DARK_GREY("8", "#555555", "\u001b[090m"),
+    DARK_GRAY("8", "#555555", "\u001b[090m"),
+    GREY("7", "#AAAAAA", "\u001b[039m"),
+    GRAY("7", "#AAAAAA", "\u001b[039m"),
 
-    BLACK("0", "#000000", "[030m"),
-    WHITE("f", "#FFFFFF", "[037m"),
+    BLACK("0", "#000000", "\u001b[030m"),
+    WHITE("f", "#FFFFFF", "\u001b[037m"),
 
     // @formatter: on
     ;
